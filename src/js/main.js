2607fa93343f2b7deedfc7af3c204ee118ee3cd0
AngelesMB <angeles.martinezbarrio@gmail.com>
2021-12-28 15:55:01 +0100

    Add first functions. Get data from api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,45 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const searchElem = document.querySelector(".js-search");
+const containerElem = document.querySelector(".js-container");
+const searchButton = document.querySelector(".js-searchButton");
+const resetButton = document.querySelector(".js-resetButton");
+
+const APIPath = "https://api.jikan.moe/v3/search/anime?q=";
+
+let showsArray = [];
+let searchValue = "";
+
+function getSearchValue() {
+  searchValue = searchElem.value.toLowerCase();
+}
+
+function getDataApi() {
+  fetch(APIPath + searchValue)
+    .then((response) => response.json())
+    .then((data) => {
+      showsArray = data.results;
+      renderShows();
+    });
+}
+
+function renderShows() {
+  containerElem.innerHTML = "";
+  for (const eachShow of showsArray) {
+    containerElem.innerHTML += `<p>${eachShow.title}</p> <img src=${eachShow.image_url}>`;
+  }
+}
+
+function handleSearchButtonClick(event) {
+  event.preventDefault();
+  getSearchValue();
+  getDataApi();
+}
+
+function handleResetButtonClick(event) {
+  //   event.preventDefault();
+  console.log("Has clickado en reset");
+}
+
+searchButton.addEventListener("click", handleSearchButtonClick);
+resetButton.addEventListener("click", handleResetButtonClick);
