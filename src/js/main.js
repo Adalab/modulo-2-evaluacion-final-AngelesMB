77db72a51d359e8ca3c8abf735bd259081bd75f3
AngelesMB <angeles.martinezbarrio@gmail.com>
2021-12-28 16:47:07 +0100

    Add setAsFavorite function

--- src/js/main.js
+++ src/js/main.js
@@ -14,25 +14,6 @@ function getSearchValue() {
   searchValue = searchElem.value.toLowerCase();
 }
 
-function getDataApi() {
-  fetch(APIPath + searchValue)
-    .then((response) => response.json())
-    .then((data) => {
-      showsArray = data.results;
-      renderShows();
-    });
-}
-
-function renderShows() {
-  containerElem.innerHTML = "";
-  for (const eachShow of showsArray) {
-    const eachShowContainer = document.createElement("div");
-    eachShowContainer.classList.add("show--preview");
-    renderTitle(eachShowContainer, eachShow);
-    renderImage(eachShowContainer, eachShow);
-  }
-}
-
 function renderTitle(div, show) {
   const titleElem = document.createElement("h4");
   const title = document.createTextNode(show.title);
@@ -47,10 +28,44 @@ function renderImage(div, show) {
   containerElem.appendChild(div);
 }
 
+function renderShows() {
+  containerElem.innerHTML = "";
+  for (const eachShow of showsArray) {
+    const eachShowContainer = document.createElement("div");
+    eachShowContainer.classList.add("show--preview");
+    renderTitle(eachShowContainer, eachShow);
+    renderImage(eachShowContainer, eachShow);
+    eachShowContainer.addEventListener("click", handleShowClick);
+  }
+}
+
+function getDataApi() {
+  fetch(APIPath + searchValue)
+    .then((response) => response.json())
+    .then((data) => {
+      showsArray = data.results;
+      renderShows();
+    });
+}
+
 function handleSearchButtonClick(event) {
   event.preventDefault();
   getSearchValue();
   getDataApi();
+}
+
+function setAsFavorite(event) {
+  const selectedShow = event.currentTarget;
+  selectedShow.classList.toggle("favorite");
+}
+
+function saveAsFavorite(event) {
+  console.log(`guardada como favorita:${event.currentTarget.textContent}`);
+}
+
+function handleShowClick(event) {
+  setAsFavorite(event);
+  saveAsFavorite(event);
 }
 
 function handleResetButtonClick(event) {
