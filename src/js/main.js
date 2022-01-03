1196dff6c15ca39cf64890599efa4b94b43ce26a
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-01-03 13:55:39 +0100

    Refactor and rename functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
 "use strict";
 
-const searchElem = document.querySelector(".js-search");
 const containerElem = document.querySelector(".js-container");
 const favContainerElem = document.querySelector(".js-favContainer");
-const searchButton = document.querySelector(".js-searchButton");
-const resetButton = document.querySelector(".js-resetButton");
 
 const APIPath = "https://api.jikan.moe/v3/search/anime?q=";
 
@@ -12,60 +9,169 @@ let showsArray = [];
 let favShowsArray = [];
 let searchValue = "";
 
-function getSearchValue() {
-  searchValue = searchElem.value.toLowerCase();
+// start app
+function startApp() {
+  getDataLS();
+  const searchButton = document.querySelector(".js-searchButton");
+  const resetButton = document.querySelector(".js-resetButton");
+  addListener(searchButton, handleSearchButtonClick);
+  addListener(resetButton, handleResetButtonClick);
 }
 
-function renderTitle(div, show) {
+// get data from ls
+function getDataLS() {
+  const favShowsArrayLS = JSON.parse(localStorage.getItem("favShowsArrayLS"));
+  if (favShowsArrayLS !== null) {
+    favShowsArray = favShowsArrayLS;
+    renderFavList();
+  }
+}
+
+// get user value after click and render results
+function handleSearchButtonClick(event) {
+  event.preventDefault();
+  showsArray = [];
+  getSearchValue();
+  if (searchValue === "") {
+    renderErrorMessage("Venga, busca algo.");
+  } else if (searchValue.length < 3) {
+    renderErrorMessage("Introduce al menos 3 caracteres.");
+  } else {
+    getDataApi().then(() => {
+      if (showsArray.length === 0) {
+        renderErrorMessage("No hemos encontrado resultados con esa búsqueda.");
+      } else {
+        renderResults();
+      }
+    });
+  }
+}
+
+// set as favorite after click and render fav list
+function handleShowClick(event) {
+  renderAsFavorite(event);
+  saveAsFavorite(event);
+  saveToLS();
+  renderFavList();
+}
+
+// delete from favorites one by one
+function handleRemoveFavClick(event) {
+  removeFavFromArray(event);
+  saveToLS();
+  renderFavList();
+}
+
+// delete all favorites
+function handleResetButtonFavClick() {
+  favShowsArray = [];
+  saveToLS();
+  renderFavList();
+  renderResults();
+}
+
+// delete all results
+function handleResetButtonClick() {
+  showsArray = [];
+  containerElem.innerHTML = "";
+}
+
+// -----------------------------------------------------------------------------
+
+function renderFavList() {
+  favContainerElem.innerHTML = "";
+  if (favShowsArray.length !== 0) {
+    for (const eachShow of favShowsArray) {
+      renderShowPreviewFav(eachShow);
+    }
+    const resetButtonFav = renderResetButtonFav();
+    addListener(resetButtonFav, handleResetButtonFavClick);
+  }
+}
+
+function ensureSearchValue() {}
+
+function renderShowPreviewFav(show) {
+  const eachShowContainer = document.createElement("div");
+  eachShowContainer.classList.add("show--preview__fav");
+  eachShowContainer.setAttribute("data-id", show.mal_id);
+  renderTitle(eachShowContainer, show);
+  renderImage(eachShowContainer, show);
+  favContainerElem.appendChild(eachShowContainer);
+  const removeFavIcon = renderRemoveFavIcon(eachShowContainer);
+  addListener(removeFavIcon, handleRemoveFavClick);
+}
+
+function renderTitle(container, show) {
   const titleElem = document.createElement("h4");
   const title = document.createTextNode(show.title);
   titleElem.classList.add("preview--title");
   titleElem.appendChild(title);
-  div.appendChild(titleElem);
+  container.appendChild(titleElem);
 }
 
-function renderImage(div, show, container) {
+function renderImage(container, show) {
   const divImg = document.createElement("div");
   divImg.classList.add("preview--image");
-  div.appendChild(divImg);
   divImg.setAttribute("style", `background-image:url(${show.image_url})`);
-  renderSynopsis(div, show, divImg, container);
+  container.appendChild(divImg);
+  return divImg;
 }
 
-function renderSynopsis(div, show, divImg, container) {
+// Mouseover synopsis ¿?
+function renderSynopsis(divImg, container, show) {
   const synopsisElem = document.createElement("p");
   const synopsis = document.createTextNode(show.synopsis);
   synopsisElem.classList.add("preview--synopsis", "hidden");
   synopsisElem.appendChild(synopsis);
   divImg.appendChild(synopsisElem);
-  container.appendChild(div);
+  container.appendChild(divImg);
 }
 
-// revisar función
-function checkIfFavorite(eachShow) {
-  if (favShowsArray !== []) {
-    const foundInFavArray = favShowsArray.find(
-      (eachFavShow) => eachFavShow.mal_id === eachShow.mal_id
-    );
-    if (foundInFavArray !== undefined) {
-      const results = document.querySelectorAll(".show--preview");
-      const resultsArray = Array.from(results);
-      for (const eachShow of resultsArray) {
-        if (parseInt(eachShow.dataset.id) === foundInFavArray.mal_id) {
-          eachShow.classList.add("favorite");
-        }
-      }
-    }
-  }
+function renderRemoveFavIcon(container) {
+  const removeFavIcon = document.createElement("i");
+  removeFavIcon.classList.add("far", "fa-times-circle", "fav--remove__icon");
+  container.appendChild(removeFavIcon);
+  return removeFavIcon;
 }
 
-function renderErrorMessage() {
+function addListener(element, handler) {
+  element.addEventListener("click", handler);
+}
+
+function renderResetButtonFav() {
+  const resetButtonFav = document.createElement("input");
+  resetButtonFav.setAttribute("type", "reset");
+  resetButtonFav.setAttribute("value", "Limpiar favoritos");
+  resetButtonFav.classList.add("main--buttonFav");
+  favContainerElem.appendChild(resetButtonFav);
+  return resetButtonFav;
+}
+
+function getSearchValue() {
+  const searchElem = document.querySelector(".js-search");
+  searchValue = searchElem.value.toLowerCase();
+}
+
+function renderErrorMessage(message) {
+  containerElem.innerHTML = "";
   const errorContainer = document.createElement("p");
-  const errorMessage = document.createTextNode(
-    "No hemos encontrado resultados con esa búsqueda."
-  );
+  const errorMessage = document.createTextNode(message);
   errorContainer.appendChild(errorMessage);
   containerElem.appendChild(errorContainer);
+}
+
+function getDataApi() {
+  return fetch(APIPath + searchValue)
+    .then((response) => response.json())
+    .then((data) => {
+      showsArray = data.results;
+    });
+}
+
+function renderResults() {
+  containerElem.innerHTML = "";
+  renderShows();
 }
 
 function renderShows() {
@@ -74,49 +180,60 @@ function renderShows() {
     eachShowContainer.classList.add("show--preview");
     eachShowContainer.setAttribute("data-id", eachShow.mal_id);
     renderTitle(eachShowContainer, eachShow);
-    renderImage(eachShowContainer, eachShow, containerElem);
-    eachShowContainer.addEventListener("click", handleShowClick);
-    checkIfFavorite(eachShow);
+    const divImg = renderImage(eachShowContainer, eachShow);
+    renderSynopsis(divImg, eachShowContainer, eachShow);
+    containerElem.appendChild(eachShowContainer);
+    addListener(eachShowContainer, handleShowClick);
+    checkIfFavorite(eachShow, eachShowContainer);
   }
 }
 
-function renderResults() {
-  containerElem.innerHTML = "";
-  if (showsArray.length === 0) {
-    renderErrorMessage();
-  } else {
-    renderShows();
+function checkIfFavorite(eachShow, eachShowContainer) {
+  const foundInFavArray = favShowsArray.find(
+    (eachFavShow) => eachFavShow.mal_id === eachShow.mal_id
+  );
+  if (foundInFavArray !== undefined) {
+    eachShowContainer.classList.add("favorite");
   }
 }
 
-function getDataApi() {
-  fetch(APIPath + searchValue)
-    .then((response) => response.json())
-    .then((data) => {
-      showsArray = data.results;
-      renderResults();
-    });
-}
-
-function handleSearchButtonClick(event) {
-  event.preventDefault();
-  getSearchValue();
-  if (searchValue === "") {
-    containerElem.innerHTML = "Venga, busca algo.";
-  } else if (searchValue.length < 3) {
-    containerElem.innerHTML = "Introduce al menos 3 caracteres.";
-  } else {
-    getDataApi();
-  }
-}
-
-function setAsFavorite(event) {
+function renderAsFavorite(event) {
   const selectedShow = event.currentTarget;
   selectedShow.classList.toggle("favorite");
 }
 
+function saveAsFavorite(event) {
+  const showFavId = parseInt(event.currentTarget.dataset.id);
+  const showFound = findIdInArray(showsArray, showFavId);
+  const showFavIndex = findIndexIdInArray(favShowsArray, showFavId);
+  if (event.currentTarget.classList.contains("favorite")) {
+    // Crear objeto ¿? map
+    favShowsArray.push(showFound);
+  } else {
+    favShowsArray.splice(showFavIndex, 1);
+  }
+}
+
+function findIdInArray(arr, id) {
+  return arr.find((show) => show.mal_id === id);
+}
+
+function findIndexIdInArray(arr, id) {
+  return arr.findIndex((show) => show.mal_id === id);
+}
+
+function saveToLS() {
+  localStorage.setItem("favShowsArrayLS", JSON.stringify(favShowsArray));
+}
+
+function removeFavFromArray(event) {
+  const showFavId = parseInt(event.currentTarget.parentNode.dataset.id);
+  const showFavIndex = findIndexIdInArray(favShowsArray, showFavId);
+  favShowsArray.splice(showFavIndex, 1);
+  setAsNotFavorite(showFavId);
+}
+
 function setAsNotFavorite(showFavId) {
-  // Por qué lo que devuelve el qsAll no permite hacer find
   if (showsArray.length !== 0) {
     const showPreviewFavs = document.querySelectorAll(".favorite");
     const showPreviewFavsArr = Array.from(showPreviewFavs);
@@ -129,99 +246,5 @@ function setAsNotFavorite(showFavId) {
   }
 }
 
-function removeFavFromArray(event) {
-  const showFavId = parseInt(event.currentTarget.parentNode.dataset.id);
-  const showFavIndex = favShowsArray.findIndex(
-    (favShow) => favShow.mal_id === showFavId
-  );
-  favShowsArray.splice(showFavIndex, 1);
-  setAsNotFavorite(showFavId);
-}
-
-function handleRemoveFavClick(event) {
-  removeFavFromArray(event);
-  saveToLS();
-  renderFavList();
-}
-
-function renderRemoveFavIcon(container) {
-  const removeFavIcon = document.createElement("div");
-  // añadir clase con espacios icono de font awesome
-  // const faIconClass = "far fa-times-circle"
-  removeFavIcon.classList.add("fav--remove__icon");
-  removeFavIcon.addEventListener("click", handleRemoveFavClick);
-  container.appendChild(removeFavIcon);
-}
-
-function handleResetButtonFavClick() {
-  favShowsArray = [];
-  saveToLS();
-  renderFavList();
-  renderResults();
-}
-
-function renderResetButtonFav() {
-  const resetButtonFav = document.createElement("input");
-  resetButtonFav.setAttribute("type", "reset");
-  resetButtonFav.setAttribute("value", "Limpiar favoritos");
-  resetButtonFav.classList.add("main--buttonFav");
-  resetButtonFav.addEventListener("click", handleResetButtonFavClick);
-  favContainerElem.appendChild(resetButtonFav);
-}
-
-function renderFavList() {
-  favContainerElem.innerHTML = "";
-  if (favShowsArray.length !== 0) {
-    renderResetButtonFav();
-    for (const eachShow of favShowsArray) {
-      const eachShowContainer = document.createElement("div");
-      eachShowContainer.classList.add("show--preview__fav");
-      eachShowContainer.setAttribute("data-id", eachShow.mal_id);
-      renderTitle(eachShowContainer, eachShow);
-      renderImage(eachShowContainer, eachShow, favContainerElem);
-      renderRemoveFavIcon(eachShowContainer);
-    }
-  }
-}
-
-function saveToLS() {
-  localStorage.setItem("favShowsArrayLS", JSON.stringify(favShowsArray));
-}
-
-function saveAsFavorite(event) {
-  const showFavId = parseInt(event.currentTarget.dataset.id);
-  const showFound = showsArray.find((show) => show.mal_id === showFavId);
-  const showFavIndex = favShowsArray.findIndex(
-    (favShow) => favShow.mal_id === showFavId
-  );
-  if (event.currentTarget.classList.contains("favorite")) {
-    favShowsArray.push(showFound);
-  } else {
-    favShowsArray.splice(showFavIndex, 1);
-  }
-  saveToLS();
-}
-
-function handleShowClick(event) {
-  setAsFavorite(event);
-  saveAsFavorite(event);
-  renderFavList();
-}
-
-function handleResetButtonClick() {
-  showsArray = [];
-  containerElem.innerHTML = "";
-}
-
-function getDataLS() {
-  const favShowsArrayLS = JSON.parse(localStorage.getItem("favShowsArrayLS"));
-  if (favShowsArrayLS !== null) {
-    favShowsArray = favShowsArrayLS;
-    renderFavList();
-  }
-}
-
-getDataLS();
-
-searchButton.addEventListener("click", handleSearchButtonClick);
-resetButton.addEventListener("click", handleResetButtonClick);
+// start app
+startApp();
